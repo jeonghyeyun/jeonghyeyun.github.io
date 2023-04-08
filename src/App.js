6035e982edbab88d115a98d7cee149f65c17472c
jeonghyeyun <102636186+jeonghyeyun@users.noreply.github.com>
2023-04-08 17:45:20 -0500

    router issues

--- src/App.js
+++ src/App.js
@@ -16,17 +16,17 @@ function App() {
   return (
     <>
     <Routes>
-      <Route path='/jeonghyeyun.github.io' element={<Home />}/>
-      <Route path='/jeonghyeyun.github.io/contact' element={<Contact />} />
-      <Route path='/jeonghyeyun.github.io/projects' element={<Projects />} />
-      <Route path='/jeonghyeyun.github.io/interests' element={<Interests />} />
-      <Route path='/jeonghyeyun.github.io/Project1' element={<Project1 />} />
-      <Route path='/jeonghyeyun.github.io/Project2' element={<Project2 />} />
-      <Route path='/jeonghyeyun.github.io/Project3' element={<Project3 />} />
-      <Route path='/jeonghyeyun.github.io/Photo' element={<Photo />} />
-      <Route path='/jeonghyeyun.github.io/Music' element={<Music />} />
-      <Route path='/jeonghyeyun.github.io/Food' element={<Food />} />
-      <Route path='/jeonghyeyun.github.io/Art' element={<Art />} />
+      <Route path='/jeonghyeyun.github.io' exact component={<Home />}/>
+      <Route path='/jeonghyeyun.github.io/contact' exact component={<Contact />} />
+      <Route path='/jeonghyeyun.github.io/projects' exact component={<Projects />} />
+      <Route path='/jeonghyeyun.github.io/interests' exact component={<Interests />} />
+      <Route path='/jeonghyeyun.github.io/Project1' exact component={<Project1 />} />
+      <Route path='/jeonghyeyun.github.io/Project2' exact component={<Project2 />} />
+      <Route path='/jeonghyeyun.github.io/Project3' exact component={<Project3 />} />
+      <Route path='/jeonghyeyun.github.io/Photo' exact component={<Photo />} />
+      <Route path='/jeonghyeyun.github.io/Music' exact component={<Music />} />
+      <Route path='/jeonghyeyun.github.io/Food' exact component={<Food />} />
+      <Route path='/jeonghyeyun.github.io/Art' exact component={<Art />} />
     </Routes>
     </>
   );
